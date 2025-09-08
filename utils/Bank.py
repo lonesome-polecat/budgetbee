258a759fc67f84e6f89fee0f22d05e1fb4869adc
Helaman Bennion <helamanb4@gmail.com>
2025-09-08 15:54:43 -0500

    add placeholder to get transactions from Bank class

--- utils/Bank.py
+++ utils/Bank.py
@@ -1,6 +1,10 @@
 # Bank.py
+import csv
 
 class Bank:
+    """
+    Holds bank information necessary for parsing transactions csv
+    """
     name: str
     amount_column: str
     category_column: str
@@ -8,5 +12,18 @@ class Bank:
     note_column: str
     post_date_column: str
 
+    cat_index: int
+    amt_index: int
+    note_index: int
+
+
     def __init__(self, **bank_json):
         self.__dict__.update(bank_json)
+
+    def set_indices(self):
+        pass
+
+    def get_transactions(self, filename):
+        print("Extracting transactions from csv...")
+        # TODO: Refactor get_transactions
+
