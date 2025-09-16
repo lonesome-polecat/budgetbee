6b41c0318315b1ca6768bb6508e088a4d5f51566
Helaman Bennion <helamanb4@gmail.com>
2025-09-16 14:22:33 -0500

    Set up pytest structure

---
+++ tests/test_example.py
@@ -0,0 +1,5 @@
+# test_example.py
+
+# Note: test functions must start with the name "test_"
+def test_addition():
+    assert 1 == 1
---
+++ utils/Transaction.py
@@ -0,0 +1,18 @@
+# Transaction.py
+
+class Transaction:
+    bank_name: str
+    amount: str
+    category: str
+    description: str
+    note: str
+    post_date: str
+
+    def __init__(self, *args):
+        self.bank_name = args[0]
+        self.amount = args[1]
+        self.category = args[2]
+        self.description = args[3]
+        self.note = args[4]
+        self.post_date = args[5]
+
